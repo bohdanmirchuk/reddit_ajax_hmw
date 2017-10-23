a9e08431c7541f133dae99ddefae8b83f9eb2a0d
bohdanmirchuk <bohdanmirchukslu@ukr.net>
2017-10-23 21:36:07 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+var get = function (url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+ 
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState != xhr.DONE) return;
+ 
+      var status = xhr.status;
+      var headers = xhr.getAllResponseHeaders();
+      var text = xhr.responseText;
+ 
+      callback(status, headers, text);
+  }
+ 
+  xhr.send();
+}
+ 
+var appendImage = function (url) {
+  var imgEl = document.createElement('img');
+  imgEl.src = url;
+  imgEl.onerror = function () {
+    imgEl.style.display = "none";
+  }
+  document.getElementById('images').appendChild(imgEl);
+}
+ 
+var getImages = function (params) {
+  params = {};
+  params.limit = params.limit || 100;
+  params.category = params.category || 'cats';
+  var url = 'https://www.reddit.com/r/pics/search.json?q=';
+  url += params.category;
+  url += '&limit='+ params.limit;
+
+  get(url, function (status, headers, body) {
+    var response = JSON.parse(body);
+    console.log(response.data.children);
+    _.each(response.data.children, function (child) {
+      var url = child.data.url;
+      appendImage(url);
+          console.log('ITEM!', child.data.url);
+        });
+
+    });
+}
+                 
